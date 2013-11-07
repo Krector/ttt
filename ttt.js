0819175b08b881948b5ab6a3b2d52f7ad9b0f122
Krector <k.rector@w3insites.com>
2013-11-07 08:23:50 -0700

    Initial Creation Tic Tac Toe

---
+++ ttt.js
@@ -0,0 +1,40 @@
+var boardArray =  [
+['', '', ''],
+['', '', ''],
+['', '', '']];
+
+var playerSymbol = ['X', 'O'];
+
+var player = 0; //X goes first
+
+var app = {
+    init: function () {
+        
+    //All the load events go here
+    $('td').click(app.verifyMove);
+        app.resetBoard();
+    },
+    
+    resetBoard : function () {
+        $('td').text('');
+        
+    },
+    
+    
+    verifyMove : function (ev) {
+       
+        var cellClicked = $(ev.target);
+        
+        if ( cellClicked.text() !== '' ){
+            alert ("invalid move");
+        }
+        else { 
+            cellClicked.text(playerSymbol[player]);
+            player = player == 0 ? 1 : 0;
+        }
+
+
+    }
+    
+    
+}
